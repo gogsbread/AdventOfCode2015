1f482ccdb8e18809d713030c2de39e73a362bca7
Antony Deepak Thomas <antonydeepak@gmail.com>
2023-11-05 22:36:10 -0800

    8

---
+++ 8.cpp
@@ -0,0 +1,48 @@
+#include "common.h"
+
+#include <openssl/md5.h>
+
+namespace {
+int p1(const std::vector<std::string> &in) {
+  int t{0};
+  for (const std::string_view s : in) {
+    int c{0};
+    for (size_t i = 0; i < s.size(); ++i) {
+      if (s[i] == '"')
+        continue;
+      if (s[i] == '\\') {
+        ++i;
+        if (s[i] == 'x') {
+          i += 2;
+        }
+      }
+      ++c;
+    }
+    t += (s.size() - c);
+  }
+  return t;
+}
+
+int p2(const std::vector<std::string> &in) {
+  int t{0};
+  for (const std::string_view s : in) {
+    int c{2};
+    for (size_t i = 0; i < s.size(); ++i) {
+      if (s[i] == '"')
+        ++c;
+      if (s[i] == '\\') {
+        ++c;
+      }
+      ++c;
+    }
+    t += (c - s.size());
+  }
+  return t;
+}
+} // namespace
+
+int main() {
+  const auto &in = gb::readIn();
+  gb::writeOut(std::to_string(p1(in)));
+  gb::writeOut(std::to_string(p2(in)));
+}
