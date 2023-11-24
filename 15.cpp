40ce8095c38abdbcf111a75918f0042b7f08f8a7
Antony Deepak Thomas <antonydeepak@gmail.com>
2023-11-23 20:29:44 -0800

    15

--- 15.cpp
+++ 15.cpp
@@ -10,28 +10,36 @@ struct Ing {
   int cal;
 };
 
-void mix(const std::vector<Ing> &ings, size_t i, int s, std::vector<int> &m,
-         int64_t &best) {
+int64_t mul(int a, int64_t acc) {
+  return a * std::max(static_cast<int64_t>(0), acc);
+}
+
+void mix(const std::vector<Ing> &ings, const size_t i, const int s,
+         std::vector<int> &m, int64_t &best) {
   if (i == ings.size() - 1) {
-    m[0] += std::max(0, (100 - s)) * ings[i].cap;
-    m[1] += std::max(0, (100 - s)) * ings[i].dur;
-    m[2] += std::max(0, (100 - s)) * ings[i].flv;
-    m[3] += std::max(0, (100 - s)) * ings[i].tex;
-    best =
-        std::max(best, std::max(static_cast<int64_t>(0),
-                                std::accumulate(m.begin(), m.end(),
-                                                static_cast<int64_t>(1),
-                                                std::multiplies<int64_t>())));
-    // std::cout << s << " " << (100 - s) << " " << best << std::endl;
+    m[0] += (100 - s) * ings[i].cap;
+    m[1] += (100 - s) * ings[i].dur;
+    m[2] += (100 - s) * ings[i].flv;
+    m[3] += (100 - s) * ings[i].tex;
+    best = std::max(best, std::accumulate(m.begin(), m.end(),
+                                          static_cast<int64_t>(1), mul));
+    m[0] -= (100 - s) * ings[i].cap;
+    m[1] -= (100 - s) * ings[i].dur;
+    m[2] -= (100 - s) * ings[i].flv;
+    m[3] -= (100 - s) * ings[i].tex;
     return;
   }
 
   for (int j{0}; j <= (100 - s); ++j) {
-    m[0] += j * ings[i].cap;
-    m[1] += j * ings[i].dur;
-    m[2] += j * ings[i].flv;
-    m[3] += j * ings[i].tex;
+    m[0] += (j * ings[i].cap);
+    m[1] += (j * ings[i].dur);
+    m[2] += (j * ings[i].flv);
+    m[3] += (j * ings[i].tex);
     mix(ings, i + 1, s + j, m, best);
+    m[0] -= (j * ings[i].cap);
+    m[1] -= (j * ings[i].dur);
+    m[2] -= (j * ings[i].flv);
+    m[3] -= (j * ings[i].tex);
   }
 }
 
@@ -40,10 +48,9 @@ int64_t p1(const std::vector<std::string> &in) {
   for (const auto &l : in) {
     Ing ing{};
     gb::sscanf(l,
-               "%s: capacity %d, durability %d, flavor %d, texture %d, "
+               "%23[^:]: capacity %d, durability %d, flavor %d, texture %d, "
                "calories %d",
                ing.name, ing.cap, ing.dur, ing.flv, ing.tex, ing.cal);
-    std::cout << ing.dur << " " << ing.tex << std::endl;
     ings.emplace_back(std::move(ing));
   }
   std::vector<int> m(4, 0);
@@ -51,9 +58,58 @@ int64_t p1(const std::vector<std::string> &in) {
   mix(ings, 0, 0, m, best);
   return best;
 }
+
+void mix2(const std::vector<Ing> &ings, const size_t i, const int s,
+          std::vector<int> &m, const int cal, int64_t &best) {
+  if (i == ings.size() - 1) {
+    if ((100 - s) * ings[i].cal == cal) {
+      m[0] += (100 - s) * ings[i].cap;
+      m[1] += (100 - s) * ings[i].dur;
+      m[2] += (100 - s) * ings[i].flv;
+      m[3] += (100 - s) * ings[i].tex;
+      best = std::max(best, std::accumulate(m.begin(), m.end(),
+                                            static_cast<int64_t>(1), mul));
+      m[0] -= (100 - s) * ings[i].cap;
+      m[1] -= (100 - s) * ings[i].dur;
+      m[2] -= (100 - s) * ings[i].flv;
+      m[3] -= (100 - s) * ings[i].tex;
+    }
+    return;
+  }
+
+  for (int j{0}; j <= std::min((100 - s), (cal / ings[i].cal)); ++j) {
+    m[0] += j * ings[i].cap;
+    m[1] += j * ings[i].dur;
+    m[2] += j * ings[i].flv;
+    m[3] += j * ings[i].tex;
+    mix2(ings, i + 1, s + j, m, cal - j * ings[i].cal, best);
+    m[0] -= j * ings[i].cap;
+    m[1] -= j * ings[i].dur;
+    m[2] -= j * ings[i].flv;
+    m[3] -= j * ings[i].tex;
+  }
+}
+
+int64_t p2(const std::vector<std::string> &in) {
+  std::vector<Ing> ings;
+  for (const auto &l : in) {
+    Ing ing{};
+    gb::sscanf(l,
+               "%23[^:]: capacity %d, durability %d, flavor %d, texture %d, "
+               "calories %d",
+               ing.name, ing.cap, ing.dur, ing.flv, ing.tex, ing.cal);
+    ings.emplace_back(std::move(ing));
+  }
+  std::vector<int> m(4, 0);
+  int64_t best{-1};
+  mix2(ings, 0, 0, m, 500, best);
+  return best;
+}
+
 } // namespace
 
 int main() {
   const auto &in = gb::readIn();
   gb::writeOut(std::to_string(p1(in)));
+  gb::writeOut(std::to_string(p2(in)));
 }
